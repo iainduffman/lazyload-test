adc37c5ad4f37bdd4eae38a92c64e96621aba47d
Iain Duff <iainrobertduff@gmail.com>
2020-11-03 21:26:07 +0400

    adding files

--- src/components/tracker.vue
+++ src/components/tracker.vue
@@ -2,7 +2,12 @@
   <div>
     <nav class="uk-background-secondary uk-light" uk-navbar>
       <div class="uk-navbar-left">
-        <a class="uk-navbar-item uk-logo" href="#">Corvid-19 Cases Tracker</a>
+        <h1 class="ml11">
+  <span class="text-wrapper">
+    <span class="line line1"></span>
+    <span class="letters">Hello Goodbye</span>
+  </span>
+</h1>
       </div>
     </nav>
     <section>
@@ -11,6 +16,7 @@
          <div v-if="loading" class="uk-align-center">
           <span uk-spinner="ratio: 4.5"></span>
         </div>
+
 
         <div class="uk-child-width-1-4@s uk-grid-match" uk-grid>
           <div v-for="(data,key) in cases" :key="key">
@@ -31,6 +37,40 @@
     </section>
   </div>
 </template>
+<style>
+.ml11 {
+  font-weight: 700;
+  font-size: 3.5em;
+}
+
+.ml11 .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml11 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 3px;
+  background-color: #fff;
+  transform-origin: 0 50%;
+}
+
+.ml11 .line1 { 
+  top: 0; 
+  left: 0;
+}
+
+.ml11 .letter {
+  display: inline-block;
+  line-height: 1em;
+}
+</style>
 <script>
 import { mapState } from "vuex";
 export default {
